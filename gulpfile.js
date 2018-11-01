4db559de85965c62788e55c591a44cf17fea46bf
psywalker <musachev@voximplant.com>
2018-11-01 19:50:33 +0300

    I added file of scss in project #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
+gulp.task('default', function() {
     return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
         .pipe(sass())
         .pipe(gulp.dest('dist/css'))
 })
 
 gulp.task('watch', function(){
-    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/scss/*/.scss', ['default']);
 
 })
